25f43c6a172cf0b68f6781d0d1983e98685a9bdd
xuanduong01vn <xuanduong01vn@gmail.com>
2024-07-02 21:47:58 +0700

    create backend

---
+++ backend/index.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import bodyParse from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app =express();
+const port =9900;
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?${process.env.MONGO_OPTIONS}`,{
+  maxPoolSize:50,
+  wtimeoutMS:2500,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database connected successfully!');
+  })
+  .catch((err) => {
+    console.log('Error connecting with error code:', err.message);
+});
